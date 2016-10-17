4ca83e7cc76ab160c51ad580094c989fa505f644
mrsscherbinin <stas.kot93@gmail.com>
2016-10-17 17:49:41 +0600

    main view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+@section('content')
+
+<div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <ul class="nav nav-pills nav-stacked">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+          </ul>
+        </div>
+        <div class="col-md-9">
+           <h1>Hello, world!</h1>
+        </div>
+      </div>
+    </div>
+    
+
+@stop
